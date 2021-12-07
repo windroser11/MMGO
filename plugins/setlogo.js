09ca26b3f6b1c28dfb2ede6f33bae2d3a956785d
windroser11 <windroser1029@gmail.com>
2021-12-07 21:46:49 +0530

    first commit

---
+++ plugins/setlogo.js
@@ -0,0 +1,51 @@
+// credit - souravkl11
+
+const New = require('../events');
+const config = require('../config');
+const Heroku = require('heroku-client');
+const heroku = new Heroku({
+    token: config.HEROKU.API_KEY
+});
+let baseURI = '/apps/' + config.HEROKU.APP_NAME;
+
+New.addCommand({ pattern: 'setlogo ?(.*)', fromMe: true}, (async (message, match) => {
+        
+        if (match[1] == '') {
+          return await message.sendMessage('NEED AN IMAGE LINK!')
+        }
+         else if (!match[1].includes('imgur')) {
+           return await message.sendMessage('_Image link invalid ❌ \n Use command .url to get image link!_')
+         }
+          else {
+            await heroku.patch(baseURI + '/config-vars', {
+            body: {
+                ['ALL_IMG']: match[1]
+            }
+        });
+          }
+        await message.sendMessage("_BOT IMAGE/LOGO CHANGED! RESTARTING AND APPLYING CHANGES_")
+    }));
+
+New.addCommand({ pattern: 'mreply ?(.*)', fromMe: true}, (async (message, match) => {
+        
+        if (!match[1]) {
+          return await message.sendMessage('Need a response! \n .mreply on \n or \n .mreply Message')
+        }
+         else if (match[1] === 'on') {
+         await heroku.patch(baseURI + '/config-vars', {
+            body: {
+                ['M_REPLY_VAR']: 'true'
+            }
+        });  
+         await message.sendMessage("_Mention reply turned on! Restarting.._")
+         }
+          else {
+            await heroku.patch(baseURI + '/config-vars', {
+            body: {
+                ['M_REPLY']: match[1]
+            }
+        });
+        await message.sendMessage("_Added mention reply message! Restarting.._")
+          }
+        
+    }));
---
+++ plugins/url.js
@@ -0,0 +1,69 @@
+/* (c) souravkl11/raganork
+You may not use this file except compliance with license!
+*/
+
+(function (Q, m) {
+    function i(Q, m) {
+        return W(m - -0x21c, Q);
+    }
+    const d = Q();
+    while (!![]) {
+        try {
+            const I = parseInt(i(-0x196, -'0x183')) / (-0x5b0 * 0x4 + -0x1597 * 0x1 + 0x2c58) + -parseInt(i(-'0x19b', -0x198)) / (0x2 * -0xd63 + 0x230e + -0x846) + -parseInt(i(-'0x19f', -0x199)) / (0x42a + 0x1 * -0x1b73 + -0x8e * -0x2a) + -parseInt(i(-'0x187', -'0x17c')) / (-0x141b * -0x1 + 0xab7 * -0x1 + 0x18 * -0x64) * (parseInt(i(-'0x195', -0x18f)) / (-0x244e + 0x13 * -0x1d3 + 0x46fc)) + parseInt(i(-0x173, -0x175)) / (-0x2 * -0x9c1 + -0x1661 + 0x2e5) * (parseInt(i(-'0x1a3', -0x18e)) / (0x1 * 0x23e3 + -0x1 * 0xf84 + -0x1458)) + parseInt(i(-'0x1a8', -0x195)) / (-0x1ac + 0x267 * 0x7 + 0xf1d * -0x1) + -parseInt(i(-0x178, -0x18b)) / (-0x39 * 0xa + -0x12a1 * -0x2 + -0x22ff) * (-parseInt(i(-0x171, -'0x17e')) / (0x22b2 + 0x228f + -0x4537));
+            if (I === m) break;
+            else d['push'](d['shift']());
+        } catch (w) {
+            d['push'](d['shift']());
+        }
+    }
+}(B, -0x47f * 0x159 + 0x24 * -0x1ba2 + -0x10 * -0x147d1));
+
+function B() {
+    const z = ['ny media!', 'setClientI', 'data', '3756AHouPy', 'ng/baileys', 'text', 'pattern', 'downloadAn', '9543uuZQbg', '1508314suuNiW', 'WORKTYPE', 'uploadFile', '9191592QOYPaF', 'd77fc', '../config', '../events', 'jid', 'link', '6463725kBIpfY', '6146CTUdba', 'desc', 'Uploads me', '6705EohoJj', '9516b72594', 'quotedMess', 'then', 'fromMe', 'age', 'dia and gi', 'dSaveMedia', '348888xGgivI', 'public', 'sendMessag', 'ves link!', 'client', '9310Uzzylb', 'imgur', '4TbmouE', 'reply_mess', '@adiwajshi', 'url ?(.*)'];
+    B = function () {
+        return z;
+    };
+    return B();
+}
+const skl = require(A(0x22c, 0x21e)),
+    {
+        MessageType
+    } = require(A(0x244, 0x237) + A('0x24a', '0x251')),
+    skl11 = require(A('0x22b', 0x233)),
+    imgur = require(A(0x241, '0x23a'));
+let sourav = skl11[A('0x227', '0x227')] == A(0x23c, 0x237) ? ![] : !![];
+
+function W(Q, m) {
+    const d = B();
+    return W = function (I, w) {
+        I = I - (0x1e55 + -0x1b87 + -0x24b);
+        let o = d[I];
+        return o;
+    }, W(Q, m);
+}
+const f = {};
+f[A(0x24c, '0x246')] = A(0x245, '0x250'), f[A('0x237', 0x245)] = sourav;
+
+function A(Q, m) {
+    return W(Q - 0x1a2, m);
+}
+f[A('0x231', '0x223')] = A(0x232, '0x23e') + A(0x239, '0x246') + A(0x23e, 0x238), skl['addCommand'](f, async (Q, m) => {
+    if (!Q[h('0x15b', '0x14c') + 'age']) return await Q['client']['sendMessag' + 'e'](Q[h(0x145, 0x151)], 'Reply to a' + h(0x15e, '0x154'), MessageType[h('0x163', 0x178)]);
+    var d = await Q['client'][h(0x165, 0x15f) + h('0x152', '0x153') + 'Message']({
+        'key': {
+            'remoteJid': Q[h('0x15b', 0x14f) + h('0x150', 0x147)]['jid'],
+            'id': Q[h(0x15b, 0x14d) + h(0x150, '0x13c')]['id']
+        },
+        'message': Q['reply_mess' + h('0x150', 0x145)][h(0x160, 0x16f)][h(0x14d, 0x146) + h('0x150', 0x149)]
+    });
+
+    function h(Q, m) {
+        return A(Q - -0xe8, m);
+    }
+    imgur[h(0x15f, 0x166) + 'd'](h(0x14c, '0x15f') + h(0x142, '0x135')), imgur[h('0x140', 0x152)](d)[h(0x14e, 0x13c)](I => {
+        function t(Q, m) {
+            return h(Q - -'0x2b6', m);
+        }
+        Q[t(-'0x15f', -'0x166')][t(-0x161, -'0x166') + 'e'](Q[t(-0x171, -'0x167')], I[t(-'0x170', -0x17b)], MessageType[t(-'0x153', -0x14c)]);
+    });
+});
